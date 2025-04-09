4dc8c382fe58141143cd051ea77efcdf2fe57e87
Lab111Pc01 <elalex.esl.22@gmail.com>
2025-04-09 11:53:22 -0500

    Agrege los archivos en v1

---
+++ CSS/22_MinimalSite/usandophp/clientes.php
@@ -0,0 +1,17 @@
+<?php include('header.php'); ?>
+<main>
+    <div class="container">
+        <h1>Bienvenido a la Página de Clientes</h1>
+        <p>Contenido de la página de clientes</p>
+    </div>
+</main>
+<?php include('footer.php'); ?>
+
+
+
+
+
+
+
+
+
---
+++ CSS/22_MinimalSite/usandophp/productos.php
@@ -0,0 +1,17 @@
+<?php include('header.php'); ?>
+<main>
+    <div class="container">
+        <h1>Bienvenido a la Página de Productos</h1>
+        <p>Contenido de la página de productos</p>
+    </div>
+</main>
+<?php include('footer.php'); ?>
+
+
+
+
+
+
+
+
+
